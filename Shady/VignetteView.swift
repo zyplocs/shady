c091a60bcbb234e784aa7806ca53a563fcb303bc
zyplocs <zyplocked@gmail.com>
2025-12-24 16:35:39 -0600

    Pipe stitchable from Metal file into a view

---
+++ Shady/VignetteView.swift
@@ -0,0 +1,48 @@
+//
+//  VignetteView.swift
+//  Shady
+//
+//  Created by Eli J on 12/23/25.
+//
+
+import SwiftUI
+
+struct VignetteModifier: ViewModifier {
+    var center01: CGPoint = .init(x: 0.5, y: 0.5)
+    var radius: CGFloat = 0.45
+    var feather: CGFloat = 0.06
+    
+    func body(content: Content) -> some View {
+        content
+            .visualEffect { content, proxy in
+                content.colorEffect(
+                    ShaderLibrary.vignette(
+                        .float2(Float(proxy.size.width), Float(proxy.size.height)),
+                        .float2(Float(center01.x), Float(center01.y)),
+                        .float(Float(radius)),
+                        .float(Float(feather))
+                    )
+                )
+            }
+    }
+}
+
+extension View {
+    func vignette(center01: CGPoint = .init(x: 0.5, y: 0.5),
+                  radius: CGFloat = 0.45,
+                  feather: CGFloat = 0.06) -> some View {
+        modifier(VignetteModifier(center01: center01, radius: radius, feather: feather))
+    }
+}
+
+struct VignetteView: View {
+    var body: some View {
+        Image(systemName: "cat")
+            .foregroundStyle(.cyan)
+            .vignette(feather: 0.2)
+    }
+}
+
+#Preview {
+    VignetteView()
+}

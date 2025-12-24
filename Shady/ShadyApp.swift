83b04be6fb32cad3c53fdf72b0042a7f08d20c90
zyplocs <zyplocked@gmail.com>
2025-12-23 21:02:00 -0600

    Initial Commit

---
+++ Shady/ShadyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadyApp.swift
+//  Shady
+//
+//  Created by Eli J on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ShadyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
